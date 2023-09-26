f720e4c329c8265b337fd6cedd004ebe6198416d
Jviafara <jealvito@gmail.com>
2023-09-26 12:55:53 -0500

    Vercel Out

--- server/server.js
+++ server/server.js
@@ -1,16 +1,10 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import 'dotenv/config';
-import express, { json } from 'express';
-import http from 'http';
+import express from 'express';
 import './config/connectDB.js';
 import router from './routes/router.js';
 const app = express();
-
-import { readdirSync } from 'fs';
-readdirSync('./routes').map((file) =>
-    app.use('/', require('./routes/' + file))
-);
 
 app.use(cors());
 app.use(express.json());
@@ -20,12 +14,7 @@ app.use(cookieParser());
 //Routes
 app.use('/api/v1', router);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
 const port = process.env.PORT || 8080;
-const server = http.createServer(app);
 
 app.listen(port, () =>
     console.log(`Server Running on  http://localhost:${port}`)
